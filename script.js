70a1dec17e0a61e2ba7d5c3718524cf12e4f49dc
Anon <104167542+Unknownblud@users.noreply.github.com>
2024-11-16 21:55:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Gestion de l'écran d'entrée
-document.getElementById("startBtn").addEventListener("click", function() {
-    document.getElementById("entryScreen").style.display = "none";
-    document.getElementById("sommaire").style.display = "block";
+// Attente du clic sur l'écran d'entrée
+document.getElementById('entry-screen').addEventListener('click', function() {
+    // Masquer l'écran d'entrée
+    document.getElementById('entry-screen').style.display = 'none';
+    
+    // Afficher le sommaire avec une transition d'opacité
+    let sommaire = document.getElementById('sommaire');
+    sommaire.classList.remove('hidden');
+    setTimeout(function() {
+        sommaire.style.opacity = 1; // Affichage du sommaire
+    }, 10); // Petits délais pour activer l'animation d'opacité
 });
